3567c6cfc92e01aa7e8750d3408a85bffc2713de
khaydara <khaydara@N-20L6PF1M8G1F>
2022-10-06 15:25:17 +0200

    Dockerized the app and configured docker-compose with redis cache

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM golang:1.19.2-alpine3.15 AS builder
+
+
+WORKDIR /app
+
+
+COPY go.mod ./
+COPY go.sum ./
+RUN go mod download
+
+
+COPY . .
+
+RUN go build -o main .
+
+
+EXPOSE 8080
+
+CMD ["./main"]
+
+
--- main.go
+++ main.go
@@ -2,55 +2,52 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"runtime"
-	"runtime/pprof"
-	"sync"
-	"time"
-
-	"github.com/gocolly/colly"
 )
 
-var wg sync.WaitGroup
+// var wg sync.WaitGroup
 
-var threadProfile = pprof.Lookup("threadcreate")
+// var threadProfile = pprof.Lookup("threadcreate")
 
-var mu sync.Mutex
+// var mu sync.Mutex
 
-func wiki2(c *colly.Collector, workerID int, workers []int) {
+// func wiki2(c *colly.Collector, workerID int, workers []int) {
 
-	runtime.LockOSThread()
-	// mu.Lock()
-	defer wg.Done()
-	c.OnHTML(".mw-parser-output", func(e *colly.HTMLElement) {
-		e.ChildAttrs("a", "href")
-		// for _, link := range links {
-		// fmt.Println(link)
-		// }
-	})
+// 	runtime.LockOSThread()
+// 	// mu.Lock()
+// 	defer wg.Done()
+// 	c.OnHTML(".mw-parser-output", func(e *colly.HTMLElement) {
+// 		e.ChildAttrs("a", "href")
+// 		// for _, link := range links {
+// 		// fmt.Println(link)
+// 		// }
+// 	})
 
-	workers[0]++
+// 	workers[0]++
 
-	// mu.Unlock()
+// 	// mu.Unlock()
 
-	runtime.UnlockOSThread()
+// 	runtime.UnlockOSThread()
 
-	// done <- ""
-}
+// 	// done <- ""
+// }
 
-func wiki(c *colly.Collector, done chan string, workerID int, workers []int) {
+// func wiki(c *colly.Collector, done chan string, workerID int, workers []int) {
 
-	c.OnHTML(".mw-parser-output", func(e *colly.HTMLElement) {
-		e.ChildAttrs("a", "href")
-		// fmt.Println(links)
-		// print()
-		// for _, link := range links {
-		// fmt.Println(link)
-		// }
-	})
+// 	c.OnHTML(".mw-parser-output", func(e *colly.HTMLElement) {
+// 		e.ChildAttrs("a", "href")
+// 		// fmt.Println(links)
+// 		// print()
+// 		// for _, link := range links {
+// 		// fmt.Println(link)
+// 		// }
+// 	})
 
-	workers[0]++
+// 	workers[0]++
 
-}
+// }
 
 func init() {
 
@@ -60,26 +57,34 @@ func init() {
 
 func main() {
 
-	c := colly.NewCollector(
-		colly.AllowedDomains("en.wikipedia.org"),
-	)
-	// done := make(chan string)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello")
+	})
 
-	start := time.Now()
-
-	workers := []int{0}
-
-	wg.Add(30000)
-	for i := 0; i < 30000; i++ {
-		// wg.Add(1)
-		// go wiki(c, done, i, workers)
-		go wiki2(c, i, workers)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
 	}
 
-	c.Visit("https://en.wikipedia.org/wiki/Web_scraping")
-	// <-done
-	wg.Wait()
+	// c := colly.NewCollector(
+	// colly.AllowedDomains("en.wikipedia.org"),
+	// )
+	// done := make(chan string)
 
-	fmt.Printf("%d workers are done in %v", workers[0], time.Since(start))
-	fmt.Println("Threads created ", threadProfile.Count())
+	// start := time.Now()
+
+	// workers := []int{0}
+
+	// wg.Add(30000)
+	// for i := 0; i < ; i++ {
+	// wg.Add(1)
+	// go wiki(c, done, i, workers)
+	// go wiki2(c, i, workers)
+	// }
+
+	// c.Visit("https://en.wikipedia.org/wiki/Web_scraping")
+	// <-done
+	// wg.Wait()
+
+	// fmt.Printf("%d workers are done in %v", workers[0], time.Since(start))
+
 }
